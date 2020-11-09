7970b50eafb73a1f7facca17b8323c5ed90b3166
Michael Simons <michael.simons@neo4j.com>
2020-11-09 23:48:02 +0100

    Add quarkus images to tck.

--- tck/run.sh
+++ tck/run.sh
@@ -16,10 +16,12 @@ java src/main/java/org/neo4j/examples/jvm/tck/util/BoltHandshaker.java localhost
 
 # Declare a string array with type
 declare -a projects=(
-  "spring-data-imperative"
-  "spring-data-reactive"
-  "spring-plain-imperative"
-  "spring-plain-reactive"
+  "quarkus-imperative"
+  "quarkus-reactive"
+#  "spring-data-imperative"
+#  "spring-data-reactive"
+#  "spring-plain-imperative"
+#  "spring-plain-reactive"
 )
 declare -t prefix=neo4j-from-the-jvm
 
@@ -29,30 +31,32 @@ declare -t prefix=neo4j-from-the-jvm
 for underTest in "${projects[@]}"; do
   {
     printf "Testing $underTest\n"
-    if [[ $underTest = spring* ]]
+
+    if [[ $underTest = quarkus* ]]
+    then
+      (cd ../$underTest && ./mvnw clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.group=neo4j-from-the-jvm -Dquarkus.container-image.tag=latest)
+      docker run --name underTest --publish=8080 -e 'QUARKUS_NEO4J_URI=bolt://neo4j:7687' --network neo4j-tck -d $prefix/$underTest &>/dev/null
+      EXPOSED_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' underTest`
+
+    elif [[ $underTest = spring* ]]
     then
       (cd ../$underTest && ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=$prefix/$underTest:latest)
       docker run --name underTest --publish=8080 -e 'SPRING_NEO4J_URI=bolt://neo4j:7687' --network neo4j-tck -d $prefix/$underTest &>/dev/null
-      EXPOSED_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' underTest`
-      printf 'Waiting for container to start'
-      until $(curl --output /dev/null --silent --head --fail-early http://localhost:$EXPOSED_PORT); do
-        printf '.'
-        sleep 0.1
-      done
-      printf '\n'
-      SPRING_NEO4J_URI=bolt://localhost:$NEO4J_BOLT HOST_UNDER_TEST=localhost:$EXPOSED_PORT ./mvnw surefire:test
-      docker rm -f underTest &>/dev/null
+
     else
       echo "No match"
     fi
+
+    EXPOSED_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' underTest`
+    printf 'Waiting for container to start'
+    until $(curl --output /dev/null --silent --head --fail-early http://localhost:$EXPOSED_PORT); do
+      printf '.'
+      sleep 0.1
+    done
+    printf '\n'
+    SPRING_NEO4J_URI=bolt://localhost:$NEO4J_BOLT HOST_UNDER_TEST=localhost:$EXPOSED_PORT ./mvnw surefire:test
+    docker rm -f underTest &>/dev/null
   } || {
     docker rm -f underTest &>/dev/null
   }
- # if [[ underTest = spring* ]] then
-	#  $underTest//mvnw spring-boot:build-image
-	  #statement2
-  # elif [ conditional expression2 ]
-  # then
-	#   statement3
-	#   statement4
 done
