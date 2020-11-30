9537ade340a09b2b1555c275345a128319c42417
Michael Simons <michael.simons@neo4j.com>
2020-11-30 18:09:33 +0100

    Add Spring Boot 2.3 SDN+OGM example.

---
+++ spring-boot23-with-sdn-ogm/src/main/java/org/neo4j/examples/jvm/spring/data/imperative/movies/MoviesController.java
@@ -0,0 +1,31 @@
+package org.neo4j.examples.jvm.spring.data.imperative.movies;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Michael J. Simons
+ */
+@RestController
+@RequestMapping("/api/movies")
+public final class MoviesController {
+
+	private final MovieRepository movieRepository;
+
+	MoviesController(MovieRepository movieRepository) {
+		this.movieRepository = movieRepository;
+	}
+
+	@GetMapping({ "", "/" })
+	public List<Movie> get() {
+		return StreamSupport.stream(movieRepository.findAll(Sort.by("title").ascending()).spliterator(), false).collect(
+			Collectors.toList());
+	}
+}
